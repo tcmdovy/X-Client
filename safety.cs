e94fad900db6b25c0ab4b7905c374cab4166b514
tcmdovy <156616886+tcmdovy@users.noreply.github.com>
2024-06-14 16:24:41 -0700

    Create safety.cs

---
+++ safety.cs
@@ -0,0 +1,65 @@
+using GorillaNetworking;
+using Photon.Pun;
+using PlayFab.ClientModels;
+using PlayFab;
+using System.Text.RegularExpressions;
+using StupidTemplate.Notifications;
+using static StupidTemplate.Menu.Main;
+using UnityEngine;
+
+
+namespace StupidTemplate.Mods
+{
+    internal class SafetyMods
+    {
+        public static void EnterSafety()
+        {
+            buttonsType = 7;
+        }
+        public static void RPCProtection()
+        {
+            bool inRoom = PhotonNetwork.InRoom;
+            if (inRoom)
+            {
+                GorillaNot.instance.rpcCallLimit = int.MaxValue;
+                PhotonNetwork.RemoveRPCs(PhotonNetwork.LocalPlayer);
+                PhotonNetwork.OpCleanActorRpcBuffer(PhotonNetwork.LocalPlayer.ActorNumber);
+                PhotonNetwork.OpCleanRpcBuffer(GorillaTagger.Instance.myVRRig);
+                PhotonNetwork.RemoveBufferedRPCs(GorillaTagger.Instance.myVRRig.ViewID, null, null);
+            }
+     
+    }
+        public static void AntiReport()
+        {
+            try
+            {
+                foreach (GorillaPlayerScoreboardLine line in GorillaScoreboardTotalUpdater.allScoreboardLines)
+                {
+                    if (line.linePlayer == NetworkSystem.Instance.LocalPlayer)
+                    {
+                        Transform report = line.reportButton.gameObject.transform;
+                        foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+                        {
+                            if (vrrig != GorillaTagger.Instance.offlineVRRig)
+                            {
+                                float D1 = Vector3.Distance(vrrig.rightHandTransform.position, report.position);
+                                float D2 = Vector3.Distance(vrrig.leftHandTransform.position, report.position);
+
+                                float threshold = 0.35f;
+
+                                if (D1 < threshold || D2 < threshold)
+                                {
+                                    PhotonNetwork.Disconnect();
+                                    RPCProtection();
+                                    NotifiLib.SendNotification("<color=grey>[</color><color=purple>ANTI-REPORT Active</color><color=grey>]</color> <color=white> you have been disconnected.</color>");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch { } // Not connected
+        }
+    }
+    }
+
